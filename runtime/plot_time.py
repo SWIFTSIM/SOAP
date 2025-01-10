140dfb6cdb7d0b272212cc1ca2de6c3eff0f4770
robjmcgibbon <robjmcgibbon@gmail.com>
2025-01-10 15:31:53 +0000

    Update plot time script

--- runtime/plot_time.py
+++ runtime/plot_time.py
@@ -1,94 +1,114 @@
+import argparse
+import sys
 import os
 
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename = '/snap8/scratch/dp004/dc-mcgi1/soap_runtime/L0100N0752_no_filter/Thermal_non_equilibrium/SOAP_uncompressed/halo_properties_0127.hdf5'
-output_dir = 'z0_L0100N0752_no_filter'
-filename = '/snap8/scratch/dp004/dc-mcgi1/soap_runtime/L0100N0752/Thermal_non_equilibrium/SOAP_uncompressed/halo_properties_0127.hdf5'
-output_dir = 'z0_L0100N0752'
-filename = '/snap8/scratch/dp004/dc-mcgi1/soap_runtime/L0100N0752/Thermal_non_equilibrium/SOAP_uncompressed/halo_properties_0064.hdf5'
-output_dir = 'z3_L0100N0752'
+parser = argparse.ArgumentParser(description="Generates plots to identify where SOAP spent its time.")
+parser.add_argument(
+    "soap_catalogue",
+    type=str,
+    help="Filepath to SOAP catalogue to analyse"
+)
+parser.add_argument(
+    "-o", "--output", 
+    type=str, 
+    default="timings", 
+    help="Name of output directory to place plots within"
+)
+args = parser.parse_args()
 
-print(output_dir)
+filename = args.soap_catalogue
+output_dir = args.output
+print(f'Output directory for plots: {output_dir}')
 os.makedirs(output_dir, exist_ok=True)
 
 with h5py.File(filename, 'r') as file:
-    n_bound = file['BoundSubhalo/NumberOfGasParticles'][:]
-    n_bound += file['BoundSubhalo/NumberOfDarkMatterParticles'][:]
-    n_bound += file['BoundSubhalo/NumberOfStarParticles'][:]
-    n_bound += file['BoundSubhalo/NumberOfBlackHoleParticles'][:]
+    # Extract some subhalo properties
+    n_bound = file['InputHalos/NumberOfBoundParticles'][:]
     is_central = file['InputHalos/IsCentral'][:] == 1
     host_idx = file['SOAP/HostHaloIndex'][:]
     host_n_bound = n_bound.copy()
     host_n_bound[~is_central] = n_bound[host_idx[~is_central]]
 
+    # Load the number of loops required to process this halo
     n_loop = file['InputHalos/n_loop'][:]
     n_process = file['InputHalos/n_process'][:]
+    # Load the total processing time for each subhalo
     process_time = file['InputHalos/process_time'][:]
-    calc_time = 0 * process_time
 
+    # Load the processing time for each halo type
+    # (e.g. time spend on SO/200_crit)
+    calc_time = 0 * process_time
     halo_props = file['Parameters'].attrs['calculations'].tolist()
     subhalo_types = file['Header'].attrs['SubhaloTypes'].tolist()
     halo_prop_times = {}
     for k in file['InputHalos'].keys():
-        if k == 'total_time':
-            continue
         if '_time' in k:
             halo_prop_times[k] = file[f'InputHalos/{k}'][:]
         if '_final_time' in k:
             calc_time += file[f'InputHalos/{k}'][:]
 
-wallclock = np.sum(process_time) / 128
-print(f'Estimated wallclock time (assuming 128 ranks): {wallclock}s')
-print(f'Max processing time: {np.max(process_time)}s')
-
+# Defining limits used by multiple plots
 xlim_lo = np.min(0.9*n_bound)
 xlim_hi = np.max(1.1*n_bound)
 ylim_lo = np.min(0.9*process_time)
 ylim_hi = np.max(1.1*process_time)
 
-def plot_time_vs_mass(cprop, cbar_label, plot_name):
+#######################
+
+print('Plotting total time to process each subhalo')
+
+def plot_time_vs_nbound(cprop, cbar_label, plot_name):
+    """
+    Plot the total time taken to process a subhalo
+    against its Nbound. Color the points according
+    to cprop.
+    """
     fig, ax = plt.subplots()
     sc = ax.scatter(n_bound, process_time, s=1, c=cprop)
     fig.colorbar(sc, ax=ax, label=cbar_label)
-    ax.set_xlabel('$N_{bound}$')
+    ax.set_xlabel('Subhalo $N_{bound}$')
     ax.set_ylabel('Time to process [s]')
     ax.set_xscale('log')
     ax.set_yscale('log')
     ax.set_xlim(xlim_lo, xlim_hi)
     ax.set_ylim(ylim_lo, ylim_hi)
-    plt.savefig(f'{output_dir}/{plot_name}.png', dpi=200)
+    plt.savefig(f'{output_dir}/ProcessTime_{plot_name}.png', dpi=200)
     plt.close()
 
-plot_time_vs_mass(
+plot_time_vs_nbound(
     calc_time / process_time,
     'Fraction of time spent doing calculation',
-    'frac_calc'
+    'FracCalc'
 )
-plot_time_vs_mass(
+plot_time_vs_nbound(
     is_central,
     'Is central?',
-    'is_central'
+    'IsCentral'
 )
-plot_time_vs_mass(
+plot_time_vs_nbound(
     n_loop,
     'Number of density loops',
-    'n_loop'
+    'Nloop'
 )
-plot_time_vs_mass(
+plot_time_vs_nbound(
     n_process,
-    'Number of times halos was processed',
-    'n_process'
+    'Number of times the subhalo was processed',
+    'Nprocess'
 )
-plot_time_vs_mass(
+plot_time_vs_nbound(
     np.log10(host_n_bound),
     'log(Host $N_{bound}$)',
-    'host_n_bound'
+    'HostNbound'
 )
 
-# Plotting total time taken by each time-mass bin
+#######################
+
+print('Plotting total processing time taken by each time-mass bin')
+
 fig, ax = plt.subplots(1)
 weights = process_time / np.sum(process_time)
 n_bound_bins = 10**np.linspace(np.log10(xlim_lo), np.log10(xlim_hi), 15)
@@ -99,21 +119,20 @@ ax.set_xlabel('$N_{bound}$')
 ax.set_ylabel('Time to process [s]')
 ax.set_xscale('log')
 ax.set_yscale('log')
-plt.savefig(f'{output_dir}/frac_time.png', dpi=200)
+plt.savefig(f'{output_dir}/SubhaloMassTimeFraction.png', dpi=200)
 plt.close()
 
-# Plot time taken for each halo type for different halo mass bins
+#######################
+
+print('Plotting time taken for each halo type, splitting results into mass bins')
+
 n_bin = 4
+
 n_bound_bins = 10**np.linspace(np.log10(xlim_lo), np.log10(xlim_hi), n_bin+1)
 fig, axs = plt.subplots(n_bin, sharex=True)
 fig.subplots_adjust(hspace=0)
 for i in range(n_bin):
     mask = (n_bound_bins[i] < n_bound) & (n_bound <= n_bound_bins[i+1])
-
-    # TODO: Mask
-    # mask &= is_central
-    # mask &= np.logical_not(is_central)
-    # mask &= host_n_bound > 10**5
 
     final_fracs, total_fracs = [], []
     for halo_prop in halo_props:
@@ -133,23 +152,29 @@ for i in range(n_bin):
 axs[n_bin-1].set_xticks(x)
 axs[n_bin-1].set_xticklabels(labels=halo_props, rotation=45, ha='right')
 fig.text(-0.05, 0.5, 'Fraction of time spent on halo type', va='center', rotation='vertical', fontsize=14)
-plt.savefig(f'{output_dir}/halo_prop_frac.png', dpi=400, bbox_inches='tight')
+plt.savefig(f'{output_dir}/HaloTypeTimeFraction.png', dpi=400, bbox_inches='tight')
 plt.close()
 
-# Plot time taken for each property for different halo mass bins
+#######################
+
+print('Plotting the time taken on each property for each halo type, splitting results into mass bins')
 # This essentially loads the entire SOAP file, so takes a while to run
+# Also saves the total time spent on calculations, which is needed for the next plot
+
 n_bin = 4
+
+total_prop_calculation time = {}
 n_bound_bins = 10**np.linspace(np.log10(xlim_lo), np.log10(xlim_hi), n_bin+1)
 with h5py.File(filename, 'r') as file:
     for group_name in subhalo_types:
-    # for group_name in ['BoundSubhalo']:
-        if 'proj' in group_name:
-            # We don't store times for individual projections
+        # We don't store times for individual projections
+        if ('proj' in group_name) or (group_name == 'InputHalos'):
             continue
 
         prop_times = {i: [] for i in range(n_bin)}
         prop_names = []
 
+        prop_calculation_time = 0 * halo_prop_times['bound_subhalo_final_time']
         for k in file[group_name]:
             if '_time' not in k:
                 continue
@@ -159,12 +184,20 @@ with h5py.File(filename, 'r') as file:
             for i_bin in range(n_bin):
                 mask = (n_bound_bins[i_bin] < n_bound) & (n_bound <= n_bound_bins[i_bin+1])
                 prop_times[i_bin].append(np.sum(arr[mask]))
+                prop_calculation_time[mask] += arr[mask]
 
         if len(prop_names) == 0:
             continue
-
         print(f'Plotting {group_name}')
-        figsize = (0.2 * len(file[group_name].keys()), 4.8)
+        total_prop_calculation_time[group_name] = prop_calcultion_time
+
+        # ProjectedAperture times are store in the top level group, but the properties
+        # themselves are stored in the individual projections. This means we have
+        # less keys, and so need to take that into account when setting the figsize.
+        if 'ProjectedAperture' in group_name:
+            figsize = (0.4 * len(file[group_name].keys()), 4.8)
+        else:
+            figsize = (0.2 * len(file[group_name].keys()), 4.8)
         fig, axs = plt.subplots(n_bin, sharex=True, figsize=figsize)
         fig.subplots_adjust(hspace=0)
 
@@ -176,10 +209,11 @@ with h5py.File(filename, 'r') as file:
         axs[n_bin-1].set_xticks(x)
         axs[n_bin-1].set_xticklabels(labels=prop_names, rotation=45, ha='right')
         # fig.text(0, 0.5, 'Time spent on property', va='center', rotation='vertical', fontsize=14)
-        plot_name = group_name.replace('/', '_')
+        plot_name = f'PropertyTime_{group_name.replace('/', '_')}'
         plt.savefig(f'{output_dir}/{plot_name}.png', dpi=400, bbox_inches='tight')
         plt.close()
 
+#######################
 
 
 
@@ -190,15 +224,7 @@ with h5py.File(filename, 'r') as file:
 
 
 
+#######################
 
-
-
-
-
-
-
-
-
-
-
+print("Done!")
 
