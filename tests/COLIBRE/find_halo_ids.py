543ae7db28ca4f121b3735cb649ae342644113c1
robjmcgibbon <robjmcgibbon@gmail.com>
2025-09-12 15:27:16 +0100

    Update tests

--- tests/COLIBRE/find_halo_ids.py
+++ tests/COLIBRE/find_halo_ids.py
@@ -10,14 +10,16 @@ import h5py
 
 
 def find_halo_indices(sim, snap_nr, boxsize):
-    soap_file = f"/cosma8/data/dp004/colibre/Runs//{sim}/SOAP/halo_properties_{snap_nr:04d}.hdf5"
+    soap_file = f"/cosma8/data/dp004/colibre/Runs//{sim}/SOAP-HBT/halo_properties_{snap_nr:04d}.hdf5"
     with h5py.File(soap_file, "r") as f:
         pos = f["InputHalos/HaloCentre"][()]
         mask = np.all(pos < boxsize, axis=1)
         index = f["InputHalos/HaloCatalogueIndex"][()]
         is_central = f["InputHalos/IsCentral"][()]
+        nstar = f['BoundSubhalo/NumberOfStarParticles'][:]
         if np.sum(is_central[mask]) == 0:
             print('No centrals loaded')
+        print(f'Max number of stars: {np.max(nstar[mask])}')
         return index[mask]
 
 
