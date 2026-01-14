03679bbe3984641a3601da47afdac30f05087eaa
robjmcgibbon <robjmcgibbon@gmail.com>
2026-01-14 09:55:12 +0000

    Identify gas progenitor

--- misc/compute_BirthHaloCatalogueIndex.py
+++ misc/compute_BirthHaloCatalogueIndex.py
@@ -1,25 +1,21 @@
 #!/bin/env python
 
 """
-# TODO: Update
-match_group_membership.py
+compute_BirthHaloCatalogueIndex.py
 
-This script matches halos between different simulations run from the
-same initial conditions.
+This script produces an auxiliary snapshot which contains the subhalo
+id each star was part of when it first formed.
 
 Usage:
 
-  mpirun -- python -u misc/match_group_membership \
-            --snap-basename1 SNAP_BASENAME1 \
-            --snap-basename2 SNAP_BASENAME2 \
-            --membership-basename1 MEMBERSHIP_BASENAME1 \
-            --membership-basename2 MEMBERSHIP_BASENAME2 \
-            --catalogue-filename1 CATALOGUE_FILENAME1 \
-            --catalogue-filename2 CATALOGUE_FILENAME2 \
-            --output-filename OUTPUT_FILENAME
+  mpirun -- python -u misc/compute_BirthHaloCatalogueIndex.py \
+            --snap-basename SNAP_BASENAME \
+            --membership-basename MEMBERSHIP_BASENAME \
+            --output-basename OUTPUT_FILENAME \
+            --final-snap-nr FINAL_SNAP_NR
 
-Run "python misc/match_group_membership.py -h" for a discription
-of the optional arguments.
+Run "python misc/compute_BirthHaloCatalogueIndex.py -h" for a full description
+of the arguments, and a list of optional arguments.
 
 """
 
@@ -77,12 +73,12 @@ def load_particle_data(snap_basename, membership_basename, load_gas, comm):
     if load_gas:
         assert (
             particle_data["PartType0/GroupNr_bound"].shape
-            == particle_data["PartType0/ParticleIds"].shape
+            == particle_data["PartType0/ParticleIDs"].shape
         )
 
     return particle_data
 
-
+# Units for the dimensionless fields we will be saving
 unit_attrs = {
     "Conversion factor to CGS (not including cosmological corrections)": [1.0],
     "Conversion factor to physical CGS (including cosmological corrections)": [1.0],
@@ -137,11 +133,9 @@ if __name__ == "__main__":
         type=int,
         required=True,
         help=(
-            # TODO:
             "Snapshot at which to load the particles"
         ),
     )
-    # TODO: Skip check gas particles
     parser.add_argument(
         "--calculate-PreBirthHaloCatalogueIndex",
         action="store_true",
@@ -171,14 +165,20 @@ if __name__ == "__main__":
     star_particle_ids = particle_data["PartType4/ParticleIDs"]
     star_birth_ids = particle_data["PartType4/GroupNr_bound"]
     star_birth_ids[:] = -99
+    star_first_snapshot = np.copy(star_birth_ids)
 
-    # TODO:
-    # For the loop we need gas particles from the previous snapshot
-    # if args.calculate_PreBirthHaloCatalogueIndex:
-    # star_prebirth_ids = np.copy(star_birth_ids)
+    if args.calculate_PreBirthHaloCatalogueIndex:
+        particle_data['PartType0/ParticleIDs'] = np.ones(0)
+        particle_data['PartType0/GroupNr_bound'] = np.ones(0)
+        star_prebirth_ids = np.copy(star_birth_ids)
 
     for snap_nr in range(0, args.final_snap_nr + 1):
+
         mpi_print(f"Loading data from snapshot {snap_nr}", comm_rank)
+        if args.calculate_PreBirthHaloCatalogueIndex:
+            # We need to keep the gas IDs from snapshot N-1
+            gas_particle_ids = particle_data['PartType0/ParticleIDs']
+            gas_group_nr = particle_data['PartType0/GroupNr_bound']
         snap_basename = args.snap_basename.format(snap_nr=snap_nr)
         membership_basename = args.membership_basename.format(snap_nr=snap_nr)
         particle_data = load_particle_data(
@@ -188,6 +188,7 @@ if __name__ == "__main__":
             comm,
         )
 
+        mpi_print(f"Matching stars", comm_rank)
         # It would be quicker to make use of the BirthScaleFactors
         # instead of checking all stars
         idx = psort.parallel_match(
@@ -202,16 +203,59 @@ if __name__ == "__main__":
             comm=comm,
         )
 
-        # TODO:
-        # if args.calculate_PreBirthHaloCatalogueIndex:
-
         has_new_birth_id = star_birth_ids == -99
         has_new_birth_id[has_new_birth_id] = idx != -1
         star_birth_ids[has_new_birth_id] = new_birth_ids
+        star_first_snapshot[has_new_birth_id] = snap_nr
+
+        if args.calculate_PreBirthHaloCatalogueIndex:
+            mpi_print(f"Matching gas", comm_rank)
+            # Identify the gas progenitor of the newly formed stars
+            gas_idx = psort.parallel_match(
+                star_particle_ids[has_new_birth_id],
+                gas_particle_ids,
+                comm=comm,
+            )
+            # The gas progenitor may not exist for all stars due
+            # to particle splitting. Note this information is
+            # recoverable if required by using the SplitTrees.
+            new_prebirth_ids = -99 * np.ones_like(new_birth_ids)
+            new_prebirth_ids[gas_idx != -1] = psort.fetch_elements(
+                gas_group_nr,
+                gas_idx[gas_idx != -1],
+                comm=comm,
+            )
+            star_prebirth_ids[has_new_birth_id] = new_prebirth_ids
 
     # Check we found a value for every star
     assert np.sum(star_birth_ids == -99) == 0
 
+    # Set up what we want to output
+    output = {
+        "BirthHaloCatalogueIndex": star_birth_ids,
+        "FirstSnapshot": star_first_snapshot,
+    }
+    attrs = {
+        "BirthHaloCatalogueIndex": {"Description": "The HaloCatalogueIndex of this particle at the first snapshot it appeared."},
+        "FirstSnapshot": {"Description": "Index of the first simulation snapshot in which the star particle is present."},
+    }
+    attrs["BirthHaloCatalogueIndex"].update(unit_attrs)
+    attrs["FirstSnapshot"].update(unit_attrs)
+    if args.calculate_PreBirthHaloCatalogueIndex:
+        output["PreBirthHaloCatalogueIndex"] = star_prebirth_ids
+        attrs["PreBirthHaloCatalogueIndex"] = {"Description": "The HaloCatalogueIndex of gas prognitor at the snapshot before the star formed. -99 if no gas progenitor is found."}
+        attrs["PreBirthHaloCatalogueIndex"].update(unit_attrs)
+
+    # Check the output directory exists
+    output_filename = (
+        args.output_basename.format(snap_nr=args.final_snap_nr) + ".{file_nr}.hdf5"
+    )
+    if comm_rank == 0:
+        output_dir = os.path.dirname(output_filename)
+        os.makedirs(output_dir, exist_ok=True)
+    comm.barrier()
+
+    # Write the output
     mpi_print("Writing output", comm_rank)
     snap_file = phdf5.MultiFile(
         final_snap_basename + ".{file_nr}.hdf5",
@@ -221,17 +265,6 @@ if __name__ == "__main__":
     elements_per_file = snap_file.get_elements_per_file(
         "ParticleIDs", group="PartType4"
     )
-    output = {"BirthHaloCatalogueIndex": star_birth_ids}
-    attrs = {"BirthHaloCatalogueIndex": {"Description": "TODO"}}
-    attrs["BirthHaloCatalogueIndex"].update(unit_attrs)
-    output_filename = (
-        args.output_basename.format(snap_nr=args.final_snap_nr) + ".{file_nr}.hdf5"
-    )
-    if comm_rank == 0:
-        output_dir = os.path.dirname(output_filename)
-        os.makedirs(output_dir, exist_ok=True)
-    comm.barrier()
-
     snap_file.write(
         output,
         elements_per_file,
@@ -241,6 +274,7 @@ if __name__ == "__main__":
         attrs=attrs,
     )
 
+    # Finished
     comm.barrier()
     mpi_print(f"Runtime: {datetime.datetime.now() - start_time}", comm_rank)
     mpi_print("Done!", comm_rank)
